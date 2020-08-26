2666cc9e24386d629dbdc20e3551a0406fe54523
Silvana Lima <silvana.vieira@hotmail.com>
2020-08-26 18:50:44 -0300

    chore: [WEB] update storybook 6.x

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,18 +1,19 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic'
 }
 
-export const Basic = () => (
-  <Main
-    title={text('Title', 'Boilerplate React with NextJS')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS, Styled Components, React Testing Library and Jest'
-    )}
-  />
-)
+export const Default: Story = (args) => <Main {...args} />
